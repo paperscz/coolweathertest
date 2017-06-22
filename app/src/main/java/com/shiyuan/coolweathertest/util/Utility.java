eea10cbc56a333b4b70164451eecac3a2c74b9b6
SkyZhang007 <zk827083320@qq.com>
2017-06-22 14:14:00 +0800

    加入显示天气信息

--- app/src/main/java/com/shiyuan/coolweathertest/util/Utility.java
+++ app/src/main/java/com/shiyuan/coolweathertest/util/Utility.java
@@ -2,9 +2,11 @@ package com.shiyuan.coolweathertest.util;
 
 import android.text.TextUtils;
 
+import com.google.gson.Gson;
 import com.shiyuan.coolweathertest.db.City;
 import com.shiyuan.coolweathertest.db.County;
 import com.shiyuan.coolweathertest.db.Province;
+import com.shiyuan.coolweathertest.gson.Weather;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -15,6 +17,21 @@ import org.json.JSONObject;
  */
 
 public class Utility {
+
+    public static Weather handleWeatherResponse(String response){
+
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent,Weather.class);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
     //处理省市信息
     public static boolean handleProvinceResponse(String response){
         if(!TextUtils.isEmpty(response)){
